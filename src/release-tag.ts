2836444bd23377abb85152ee8b7ecf803a9d7173
Rabbit <yfhj1990@hotmail.com>
2019-09-04 17:37:19 +0800

    Update git command

--- src/release-tag.ts
+++ src/release-tag.ts
@@ -1,10 +1,12 @@
 import * as core from '@actions/core'
 import { exec } from '@actions/exec'
+import getExecResult from './exec-result'
 import * as io from '@actions/io'
 import * as path from 'path'
 
 const DEFAULT_CONTEXT: string = '.'
-const COMMAND_GIT_FETCH_TAGS: string = 'git fetch --tags'
+const COMMAND_GIT_LAST_TAG: string = 'git tag --sort=v:refname | tail -n1'
+const COMMAND_GIT_CHECKOUT: string = 'git checkout'
 const COMMAND_NPM_VERSION: string = 'npm --no-git-tag-version version from-git'
 const COMMAND_NPM_PUBLISH: string = 'npm publish'
 
@@ -17,7 +19,8 @@ export default async function main() {
     core.debug(`Context: ${context}`)
     const isCurrentContext = context === '.'
 
-    await exec(COMMAND_GIT_FETCH_TAGS)
+    const tag = await getExecResult(COMMAND_GIT_LAST_TAG)
+    await exec(COMMAND_GIT_CHECKOUT + ' ' + tag)
     await exec(COMMAND_NPM_VERSION)
     
     if(!isCurrentContext) {
